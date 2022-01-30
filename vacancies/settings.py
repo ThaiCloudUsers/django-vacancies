d51cd2b38c968baa68da29d3465de1cd22faf031
Webmaster <webmaster@thaicloudusers.com>
2022-01-30 15:33:42 +0700

    Added Python-dotenv

--- vacancies/settings.py
+++ vacancies/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+# Load Environment Variables from file (.env)
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
